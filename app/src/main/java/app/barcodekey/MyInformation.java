f2a69c009e97384e85c4684bf879804df20f38c7
Sami Korhonen <sami.korhonen@helsinki.fi>
2014-09-09 15:00:01 +0300

    Lomakkeen valmistelua

---
+++ app/src/main/java/app/barcodekey/MyInformation.java
@@ -0,0 +1,53 @@
+package app.barcodekey;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import app.barcodekey.R;
+
+public class MyInformation extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_information);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my_information, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void saveInformation(View view){
+        Intent intent = new Intent(this, Main_menu.class); // Asetuksiin
+//        EditText editText = (EditText) findViewById(R.id.edit_message);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void backToMainMenu(View view) {
+        Intent intent = new Intent(this, Main_menu.class); // Takas alkuun
+        startActivity(intent);
+    }
+}
