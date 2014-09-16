59deef2d36468810d1dff6c86d1ab9dcab1385c1
esakorte <esa.kortelainen@helsinki.fi>
2014-09-16 13:36:58 +0300

    pari testiluokan alkua

---
+++ app/src/androidTest/java/app/barcodekey/Main_menuTest.java
@@ -0,0 +1,12 @@
+package app.barcodekey;
+
+import android.test.InstrumentationTestCase;
+
+public class Main_menuTest extends InstrumentationTestCase {
+
+    public void test() throws Exception {
+        final int expected = 1;
+        final int reality = 1;
+        assertEquals(expected, reality);
+    }
+}
---
+++ app/src/androidTest/java/app/barcodekey/SettingsTest.java
@@ -0,0 +1,12 @@
+package app.barcodekey;
+
+import android.test.InstrumentationTestCase;
+
+public class SettingsTest extends InstrumentationTestCase {
+
+    public void test() throws Exception {
+        final int expected = 1;
+        final int reality = 1;
+        assertEquals(expected, reality);
+    }
+}
