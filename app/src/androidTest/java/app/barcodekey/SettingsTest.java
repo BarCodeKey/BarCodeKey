87f74a9d5b1f61dc1c740f8eed17c24e7bb14093
esakorte <esa.kortelainen@helsinki.fi>
2014-09-16 15:39:27 +0300

    testitesti

--- app/src/androidTest/java/app/barcodekey/SettingsTest.java
+++ app/src/androidTest/java/app/barcodekey/SettingsTest.java
@@ -6,7 +6,7 @@ public class SettingsTest extends InstrumentationTestCase {
 
     public void test() throws Exception {
         final int expected = 1;
-        final int reality = 1;
+        final int reality = 5;
         assertEquals(expected, reality);
     }
 }
