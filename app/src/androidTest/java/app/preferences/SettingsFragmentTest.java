30efacf95031215807687a50608f96b3778ce439
esakorte <esa.kortelainen@helsinki.fi>
2014-11-04 13:01:46 +0200

    settings ei enää hätäänny näytön kääntymisestä

--- app/src/androidTest/java/app/preferences/SettingsFragmentTest.java
+++ app/src/androidTest/java/app/preferences/SettingsFragmentTest.java
@@ -4,5 +4,15 @@ import android.test.InstrumentationTestCase;
 
 public class SettingsFragmentTest extends InstrumentationTestCase {
 
+    SettingsFragment testFragment;
+
+    @Override
+    protected void setUp() throws Exception {
+        testFragment = new SettingsFragment();
+    }
+
+    public void testFirstName() throws Exception {
+
+    }
 
 }
