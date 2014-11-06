933ea4073512d9393f65e59dbaccdbed28281924
Viivi <vnissila@cs.helsinki.fi>
2014-11-06 20:22:43 +0200

    tehty testiluokka Bind:in testaukseen

---
+++ app/src/androidTest/java/app/security/RemoteServiceTest.java
@@ -0,0 +1,21 @@
+package app.security;
+
+
+import android.content.Intent;
+import android.os.IBinder;
+import android.test.ServiceTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
+
+public class RemoteServiceTest extends ServiceTestCase<RemoteService> {
+
+    public RemoteServiceTest() {
+        super(RemoteService.class);
+    }
+
+
+    @MediumTest
+    public void testBindable() {
+        Intent startIntent = new Intent("app.security.RemoteService");
+        IBinder service = bindService(startIntent);
+    }
+}
