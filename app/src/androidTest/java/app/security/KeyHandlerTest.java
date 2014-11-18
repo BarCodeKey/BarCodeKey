7450056e49826d50aa983ba3e648752b9cf32e53
Sami Korhonen <sami.korhonen@helsinki.fi>
2014-11-18 10:28:38 +0200

    KeyHandlerin testaus kuntoon

--- app/src/androidTest/java/app/security/KeyHandlerTest.java
+++ app/src/androidTest/java/app/security/KeyHandlerTest.java
@@ -3,6 +3,8 @@ package app.security;
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
 
+import java.security.KeyPair;
+
 import app.barcodekey.MainMenu;
 
 public class KeyHandlerTest extends ActivityInstrumentationTestCase2<MainMenu> {
@@ -14,39 +16,46 @@ public class KeyHandlerTest extends ActivityInstrumentationTestCase2<MainMenu> {
     public void testSetPrivateKey() throws Exception {
         Activity activity = getActivity();
         KeyHandler kh = new KeyHandler(activity);
-        kh.setPrivateKey("kilpikonna");
-        assertEquals("kilpikonna", kh.getPrivateKey());
+        KeyPair kp = kh.createKeys();
+        String privateKey = KeyHandler.base64Encode(kp.getPrivate().getEncoded());
+        assertNotNull(privateKey);
     }
 
     public void testSetPublicKey() throws Exception {
         Activity activity = getActivity();
         KeyHandler kh = new KeyHandler(activity);
-        kh.setPublicKey("virtahepo");
-        assertEquals("virtahepo", kh.getPublicKey());
+        KeyPair kp = kh.createKeys();
+        String publicKey = KeyHandler.base64Encode(kp.getPublic().getEncoded());
+        assertNotNull(publicKey);
     }
 
     public void testCreateKeysCreatesSomething() throws Exception {
         Activity activity = getActivity();
         KeyHandler kh = new KeyHandler(activity);
-        String key = kh.createKeys();
-        assertNotSame("Keymaking failed", key);
+        KeyPair kp = kh.createKeys();
+        String publicKey = KeyHandler.base64Encode(kp.getPublic().getEncoded());
+        assertNotSame("Keymaking failed", publicKey);
     }
 
     public void testCreateKeysChangesPublicKey() throws Exception {
         Activity activity = getActivity();
         KeyHandler kh = new KeyHandler(activity);
-        String firstKey = kh.getPublicKey();
-        kh.createKeys();
-        String secondKey = kh.getPublicKey();
+        KeyPair kp = kh.createKeys();
+        String firstKey = KeyHandler.base64Encode(kp.getPublic().getEncoded());
+
+        kp = kh.createKeys();
+        String secondKey = KeyHandler.base64Encode(kp.getPublic().getEncoded());
         assertNotSame(firstKey, secondKey);
     }
 
     public void testCreateKeysChangesPrivateKey() throws Exception {
         Activity activity = getActivity();
         KeyHandler kh = new KeyHandler(activity);
-        String firstKey = kh.getPrivateKey();
-        kh.createKeys();
-        String secondKey = kh.getPrivateKey();
+        KeyPair kp = kh.createKeys();
+        String firstKey = KeyHandler.base64Encode(kp.getPrivate().getEncoded());
+
+        kp = kh.createKeys();
+        String secondKey = KeyHandler.base64Encode(kp.getPrivate().getEncoded());
         assertNotSame(firstKey, secondKey);
     }
 }
