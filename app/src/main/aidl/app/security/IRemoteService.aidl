34bf75bd24a90d1cdefb1f9f40a984140ca01dd2
Sami Korhonen <sami.korhonen@helsinki.fi>
2014-12-02 13:25:26 +0200

    Putki kuntoon

--- app/src/main/aidl/app/security/IRemoteService.aidl
+++ app/src/main/aidl/app/security/IRemoteService.aidl
@@ -4,7 +4,6 @@ package app.security;
 
 interface IRemoteService {
 
-
     byte[] encrypt(String keyType,in byte[] data, String lookupKey);
     byte[] decrypt(String keyType,in byte[] data, String lookupKey);
 }
--- app/src/main/java/app/security/RemoteService.java
+++ app/src/main/java/app/security/RemoteService.java
@@ -26,28 +26,57 @@ public class RemoteService extends Service {
     public static final String EXTRA_PACKAGE_NAME = "package_name";
     private String appName;
 
+    private final IRemoteService.Stub mBinder = new IRemoteService.Stub(){
+
+        @Override
+        public byte[] encrypt(String keyType, byte[] data, String lookupKey){
+            System.out.println("kryptataan: " + data);
+            System.out.println("keyType: " + keyType);
+            System.out.println("lookupKey: " + lookupKey);
+            try {
+                return "peruna".getBytes();
+                //    return CryptoHandler.encryptHandler(keyType,data,getPassphrase(lookupKey));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+        @Override
+        public byte[] decrypt(String keyType,byte[] data,String lookupKey){
+            try {
+                return CryptoHandler.decryptHandler(keyType,data,getPassphrase(lookupKey));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+    };
+
     @Override
     public void onCreate() {
         super.onCreate();
-
+        System.out.println("remoteServicen onCreate!");
         sh = new SharedPreferencesService(this.getApplicationContext());
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-
+        System.out.println("remoteServicen onBind!!");
         if(intentCheck(intent)) {
+            System.out.println("tää tulee vielä?");
             appName = intent.getStringExtra(EXTRA_PACKAGE_NAME);
-            System.out.println("TÄÄLLÄ OLLAAN" + appName);
-            accept();
+            System.out.println("TÄÄLLÄ OLLAAN: " + appName);
+        //   return accept();
 
         }
-        return null;
+        return mBinder;
     }
     public Boolean intentCheck(Intent intent){
         System.out.println("checkataan!!!");
         if(intent.hasExtra(Intent.EXTRA_UID) && intent.hasExtra(EXTRA_PACKAGE_NAME)){
-            System.out.println("TÄSTÄ LÄPI!!!" + intent.getIntExtra(Intent.EXTRA_UID,0));
+            System.out.println("checkkaus läpi!!! " + intent.getIntExtra(Intent.EXTRA_UID,0));
                 return true;
 
         }
@@ -65,31 +94,6 @@ public class RemoteService extends Service {
         return mBinder;
     }
 
-   private final IRemoteService.Stub mBinder = new IRemoteService.Stub(){
-
-       @Override
-       public byte[] encrypt(String keyType,byte[] data, String lookupKey){
-
-           try {
-               return CryptoHandler.encryptHandler(keyType,data,getPassphrase(lookupKey));
-           } catch (Exception e) {
-               e.printStackTrace();
-           }
-           return null;
-       }
-       @Override
-       public byte[] decrypt(String keyType,byte[] data,String lookupKey){
-           try {
-               return CryptoHandler.decryptHandler(keyType,data,getPassphrase(lookupKey));
-           } catch (Exception e) {
-               e.printStackTrace();
-           }
-           return null;
-       }
-
-
-
-   };
 
     public String getPassphrase(String lookupKey) throws NoSuchAlgorithmException, InvalidKeyException, InvalidKeySpecException, NoSuchProviderException {
         // TODO: DOES THIS WORK??????
